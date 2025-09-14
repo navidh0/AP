b848ad2cdb7542fe0e8257baded86b859c35802a
mrkha <mr.khanbabaie@gmail.com>
2025-09-14 17:49:47 +0330

    Adding the urls

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,30 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # Django’s default admin site
     path('admin/', admin.site.urls),
+
+    # Landing page (root URL)
+    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+
+    # App routes
+    path('users/', include(('users.urls', 'users'), namespace='users')),
+    path('doctor/', include(('doctor.urls', 'doctor'), namespace='doctor')),
+    path('wallet/', include(('wallet.urls', 'wallet'), namespace='wallet')),
+    path('booking/', include(('booking.urls', 'booking'), namespace='booking')),
 ]
+
+# During development
+if settings.DEBUG:
+
+    # django-browser-reload for auto-reloading
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+    
+    # Serve media files 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
