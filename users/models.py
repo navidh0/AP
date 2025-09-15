c43298956252c5d012b2bbebd3f4f9f5caf276e7
mrkha <mr.khanbabaie@gmail.com>
2025-09-15 18:21:35 +0330

    adding the user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ("admin", "Admin"),
+        ("doctor", "Doctor"),
+        ("patient", "Patient"),
+    ]
+    GENDER_CHOICES = [
+        ("male", "Male"),
+        ("female", "Female"),
+    ]
+
+    # Extra fields
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="patient")
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    ssn = models.CharField(max_length=10, null=True, blank=True)   # ⚠ for real-world use, secure it!
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, default="male")
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
